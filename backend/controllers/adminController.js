d4f5db339401522d5b75721b55e16e7bb058b3a4
Rohit8825 <rohit.20224124@mnnit.ac.in>
2025-07-06 20:39:40 +0530

    commit

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -7,7 +7,7 @@ const addDoctor=async (req,res)=>{
 
     try {
        const {name,email,password,speciality,degree,experience,about,fees,address} =req.body
-        const imageFile=req.imageFile
+        const imageFile=req.file
 
         //checking for all data to add doctor
         if(!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address ){
